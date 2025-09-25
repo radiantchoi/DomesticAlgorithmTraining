444a6f8f9d7c0d3fe8944054bb0ea8d4e32b32ce
Gordon Choi <80261919+radiantchoi@users.noreply.github.com>
2025-09-25 18:57:19 +0900

    [level 2] Title: 모음 사전, Time: 3.35 ms, Memory: 9.62 MB -BaekjoonHub

--- Python3/프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ Python3/프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,15 +1,28 @@
-from itertools import product
-
 def solution(word):
     vowels = ["A", "E", "I", "O", "U"]
     words = []
+    existance = {}
+    found = False
     
-    for i in range(1, 6):
-        selections = product(vowels, repeat=i)
+    for vowel in vowels:
+        if found:
+            continue
         
-        for selection in selections:
-            words.append("".join(list(selection)))
+        stack = [vowel]
+        
+        while stack:
+            current = stack.pop()
+            
+            if not existance.get(current, False):
+                existance[current] = True
+                words.append(current)
+                
+                if current == word:
+                    found = True
+                    break
+            
+            if len(current) < 5:
+                for trailing in reversed(vowels):
+                    stack.append(current + trailing)
     
-    words.sort()
-    
-    return words.index(word) + 1
+    return len(words)
