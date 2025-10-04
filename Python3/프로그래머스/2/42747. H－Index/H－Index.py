f99e7e3f7b04f878a278ab8d6618ac3bba531072
Gordon Choi <80261919+radiantchoi@users.noreply.github.com>
2025-10-04 15:54:26 +0900

    [level 2] Title: H-Index, Time: 0.20 ms, Memory: 9.3 MB -BaekjoonHub

---
+++ Python3/프로그래머스/2/42747. H－Index/H－Index.py
@@ -0,0 +1,8 @@
+def solution(citations):
+    citations.sort(reverse=True)
+    
+    for (i, citation) in enumerate(citations):
+        if i + 1 > citation:
+            return i
+    
+    return len(citations)
