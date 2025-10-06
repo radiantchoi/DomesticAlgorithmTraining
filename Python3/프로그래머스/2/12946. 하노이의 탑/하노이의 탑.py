7564d79d94f47b16bbf20d1d85fba4cd31dcdc2a
Gordon Choi <80261919+radiantchoi@users.noreply.github.com>
2025-10-06 15:44:09 +0900

    [level 2] Title: 하노이의 탑, Time: 12.46 ms, Memory: 17.8 MB -BaekjoonHub

--- Python3/프로그래머스/2/12946. 하노이의 탑/하노이의 탑.py
+++ Python3/프로그래머스/2/12946. 하노이의 탑/하노이의 탑.py
@@ -1,11 +1,11 @@
-def hanoi(n, starting, mid, ending, answer):
+def hanoi(n, starting, passing, ending, answer):
     if n == 1:
         answer.append([starting, ending])
         return
     
-    hanoi(n - 1, starting, ending, mid, answer)
+    hanoi(n - 1, starting, ending, passing, answer)
     answer.append([starting, ending])
-    hanoi(n - 1, mid, starting, ending, answer)
+    hanoi(n - 1, passing, starting, ending, answer)
 
 def solution(n):
     answer = []
