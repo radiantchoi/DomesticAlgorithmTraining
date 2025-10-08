8264a90068f739ab25f7c5fb02e6a43f7bbc39cd
Gordon Choi <80261919+radiantchoi@users.noreply.github.com>
2025-10-08 23:32:22 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 0.36 ms, Memory: 9.44 MB -BaekjoonHub

--- Python3/프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.py
+++ Python3/프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.py
@@ -1,3 +1,67 @@
+from itertools import permutations as permu
+
+def spread(expression):
+    stack = []
+    current = []
+    
+    for letter in expression:
+        if letter.isdecimal():
+            current.append(letter)
+        else:
+            stack.append("".join(current))
+            current = []
+            stack.append(letter)
+    
+    stack.append("".join(current))
+    
+    return stack
+
+def register(components, rank):
+    stack = []
+    operators = []
+    
+    for component in components:
+        if component.isdecimal():
+            stack.append(component)
+        else:
+            while operators and rank[operators[-1]] >= rank[component]:
+                stack.append(operators.pop())
+            
+            operators.append(component)
+    
+    while operators:
+        stack.append(operators.pop())
+    
+    return stack
+
+def resolve(registered):
+    stack = []
+    
+    for component in registered:
+        if component.isdecimal():
+            stack.append(int(component))
+        else:
+            trailing = stack.pop()
+            leading = stack.pop()
+            
+            if component == "+":
+                stack.append(leading + trailing)
+            elif component == "-":
+                stack.append(leading - trailing)
+            else:
+                stack.append(leading * trailing)
+    
+    return abs(stack[0])
+
 def solution(expression):
     answer = 0
+
+    components = spread(expression)
+    cases = list(permu(["+", "-", "*"], 3))
+    
+    for case in cases:
+        rank = {y: x for x, y in enumerate(case)}
+        registered = register(components, rank)
+        answer = max(answer, resolve(registered))
+    
     return answer
