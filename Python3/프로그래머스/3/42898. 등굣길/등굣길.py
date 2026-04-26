3ae7b807396946638ef8d3e30e4cbf8a42878cf6
Gordon Choi <80261919+radiantchoi@users.noreply.github.com>
2026-04-26 19:17:15 +0900

    [level 3] Title: 등굣길, Time: 2.62 ms, Memory: 9.38 MB -BaekjoonHub

--- Python3/프로그래머스/3/42898. 등굣길/등굣길.py
+++ Python3/프로그래머스/3/42898. 등굣길/등굣길.py
@@ -1,21 +1,25 @@
-from collections import deque
-
 def solution(m, n, puddles):
-    grid = [[0 for _ in range(m)] for _ in range(n)]
-    grid[0][0] = 1
+    grid = [[0] * m for _ in range(n)]
     
     for puddle in puddles:
-        grid[puddle[1] - 1][puddle[0] - 1] = -1
+        grid[puddle[1]-1][puddle[0]-1] = -1
+        
+    grid[0][0] = 1
     
-    for i in range(n):
-        for j in range(m):
-            if grid[i][j] == -1:
+    for k in range(n):
+        for l in range(m):
+            if grid[k][l] < 0:
                 continue
-            
-            if (0 <= i - 1 < n) and grid[i-1][j] > -1:
-                grid[i][j] += grid[i-1][j]
-            
-            if (0 <= j - 1 < m) and grid[i][j-1] > -1:
-                grid[i][j] += grid[i][j-1]
-            
-    return grid[n - 1][m - 1] % 1_000_000_007
+            elif k == 0 and l == 0:
+                continue
+            else:
+                top_candidate = grid[k-1][l]
+                left_candidate = grid[k][l-1]
+                
+                if top_candidate >= 0:
+                    grid[k][l] += top_candidate
+                
+                if left_candidate >= 0:
+                    grid[k][l] += left_candidate
+    
+    return grid[-1][-1] % 1_000_000_007
